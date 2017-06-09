d15e87dedd613e1ce70fd7c3291046cba2f53b44
FikruMengesha <fikrumengesha@gmail.com>
2017-06-09 11:53:46 -0500

    project created

---
+++ airlinesWebApp/src/main/java/edu/mum/cs545/ws/AirlineRest.java
@@ -0,0 +1,18 @@
+package edu.mum.cs545.ws;
+
+
+
+
+
+/*@Named
+@Path("airline")*/
+public class AirlineRest {
+	/*@Inject
+	private AirlineService airlineService;
+	 @GET
+	    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	    public List<Airline> getEmployees_JSON() {
+	        List<Airline> listOfAirlines = airlineService.findAll();
+	        return listOfAirlines;
+	    }*/
+}
